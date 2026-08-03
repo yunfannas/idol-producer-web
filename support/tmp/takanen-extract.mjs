dbbdffbec2c2c4612d6397f891b7e3d148819dbd
yunfannas <yunfannas@gmail.com>
2026-08-03 14:49:47 -0700

    Move reality-data tooling to idol-data-lab

--- support/tmp/takanen-extract.mjs
+++
@@ -1,7 +0,0 @@
-import fs from "node:fs";
-const h = fs.readFileSync("tmp/takanen-event-sample.html", "utf8");
-const m = h.match(/class="entry-content[^"]*"[^>]*>([\s\S]{0,4000})/i);
-console.log(m ? m[1] : "no match");
-const idx = h.indexOf("▼会場");
-console.log("\n--- around 会場 ---");
-console.log(h.slice(idx - 50, idx + 300));
